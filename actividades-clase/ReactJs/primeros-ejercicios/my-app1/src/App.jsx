41024121241cf2522d6cace29cf66e88b6a0d1dc
Ulises Valentín Pregot <97376@sistemas.frc.utn.edu.ar>
2024-06-19 09:34:38 -0300

    jkfdjfskj

--- actividades-clase/ReactJs/primeros-ejercicios/my-app1/src/App.jsx
+++ actividades-clase/ReactJs/primeros-ejercicios/my-app1/src/App.jsx
@@ -1,19 +1,21 @@
+import React from 'react';
 import Card from "./components/Card";
+import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import "./components/Card.css";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div>
-          <h1>Estoy practicando la libreria de react</h1>
-          <div className="container container-fluid">
-            <Card titulo={"la tengo re larga hno"} sub={"literalmente no estoy mintiendo"}
-            texto={"bueno capaz que si xddddddddddd"}></Card>
-            <Card titulo={"la tengo re larga hno"} sub={"literalmente no estoy mintiendo"} />
-          </div>
+      <NavBar />
+      <div className="container-fluid">
+        <h1 className="h1">Estoy practicando la libreria de react</h1>
+        <div className="contenedor-card">
+          <Card titulo={"un titulo para una carta"} sub={"un subtitulo de una card"} texto={"un largo parrafo para una carta"} />
+          <Card titulo={"budismo para principiantes"} sub={"author: lamma yeshe"} texto={"No hace mucho, Occidente prácticamente desconocía el budismo. En la década de los cincuenta y los sesenta, por ejemplo, podrías haberte pasado la vida entera sin apenas oírlo mencionar"} />
+          <Card titulo={"cuando ya no quedan chocolates"} sub={"author: Rey David"} />
+          <Card titulo={"cuarta carta"} sub={"subtitulo"} texto={"contenido de la cuarta carta"} />
+          <Card titulo={"quinta carta"} sub={"subtitulo"} texto={"contenido de la quinta carta"} />
         </div>
       </div>
     </>
