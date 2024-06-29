6a4ca9952992e9bd1ae961aeaeb273f5a575c1fa
Ulises Valentín Pregot <97376@sistemas.frc.utn.edu.ar>
2024-06-29 20:12:58 -0300

    fda

--- actividades-clase/ReactJs/primeros-ejercicios/my-app1/src/App.jsx
+++ actividades-clase/ReactJs/primeros-ejercicios/my-app1/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <div className="contenedor-card">
           <Card titulo={"un titulo para una carta"} sub={"un subtitulo de una card"} texto={"un largo parrafo para una carta"} />
           <Card titulo={"budismo para principiantes"} sub={"author: lamma yeshe"} texto={"No hace mucho, Occidente prácticamente desconocía el budismo. En la década de los cincuenta y los sesenta, por ejemplo, podrías haberte pasado la vida entera sin apenas oírlo mencionar"} />
-          <Card titulo={"cuando ya no quedan chocolates"} sub={"author: Rey David"  } />
+          <Card titulo={"cuando ya no quedan chocolates"} sub={"author: Rey David"} />
           <Card titulo={"cuarta carta"} sub={"subtitulo"} texto={"contenido de la cuarta carta"} />
           <Card titulo={"quinta carta"} sub={"subtitulo"} texto={"contenido de la quinta carta"} />
         </div>
